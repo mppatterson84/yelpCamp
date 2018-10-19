84de5eac6c4763cd1e088f254d2ce67d1d3cb454
Michael Patterson <mppatterson84@gmail.com>
2018-10-19 01:36:13 +0000

    add db failsafe

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb://mike:hell0see@ds013222.mlab.com:13222/yelpcampmike");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12Deployed"; //failsafe database url
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
