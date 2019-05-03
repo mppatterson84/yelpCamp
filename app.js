be8dfaac8686f6c443c727c620821efe673abdcf
mppatterson84 <mppatterson84@gmail.com>
2019-05-03 11:27:30 -0500

    update app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+var listener = app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
+    console.log(`http://localhost:${listener.address().port}/campgrounds`);
 });
